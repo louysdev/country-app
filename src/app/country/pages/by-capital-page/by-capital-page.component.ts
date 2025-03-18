f0c5ee04c581ea582dfc45d880476f2276774d0e
LouysDev <louysdev@gmail.com>
2025-03-18 09:58:32 -0400

    Creación de toda la UI base

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountrySearchInputComponent } from '../../components/country-search-input/country-search-input.component';
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
   templateUrl: './by-capital-page.component.html',
+  imports: [CountryListComponent, CountrySearchInputComponent],
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+  onSearch(value: string) {
+    console.log(value);
+  }
+}
